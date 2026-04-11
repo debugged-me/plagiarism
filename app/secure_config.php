0ab100e47e5d0a72428f28180e33786d9da7a497
Clark Steven <edongclarksteven@gmail.com>
2026-04-11 08:27:29 +0800

    Update secure_config.php

--- app/secure_config.php
+++ app/secure_config.php
@@ -2,5 +2,5 @@
 
 declare(strict_types=1);
 
-const WINSTON_API_KEY = '6b2lIHOItVmE1gOiDlme6Rch3dOcnlRMlwju27rj504cd711';
+const WINSTON_API_KEY = 'XvgLRXI7vzOBITna6JeyWJP4KOp5DFntdmwfpHgs42652442';
 const TURNSTILE_SECRET_KEY = '0x4AAAAAACu7HPXs3nrAwQsv5xTLRzMy2vo';
